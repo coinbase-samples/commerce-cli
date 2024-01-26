720b62d9323610b62dcc705d01b72079d0251702
Patrick Hughes <patrick.hughes@coinbase.com>
2024-01-26 15:32:51 -0600

    create helper functions for json responses and charge creation

--- cmd/utils.go
+++ cmd/utils.go
@@ -1,5 +1,13 @@
 package cli
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/coinbase-samples/commerce-sdk-go"
+)
+
 var ChargesLongDescription = `Interact with the Coinbase Commerce charges endpoint to create and view charges. Use --setPrice to create a new charge with a specified USD amount. The --get flag requires a charge_id to retrieve a specific charge.
 
 Examples:
@@ -17,3 +25,40 @@ Examples:
 
 Events are displayed in JSON format.
 `
+
+func EventToJSON(e *commerce.SingleEvent) {
+	eventJson, err := json.MarshalIndent(e, "", " ")
+	if err != nil {
+		log.Fatalf("error marshalling events into JSON \n all events: %v ", e)
+	}
+	fmt.Printf("event %s found \n", string(eventJson))
+
+}
+
+func EventsToJSON(e *commerce.EventResponse) {
+	eventsJson, err := json.MarshalIndent(e, "", " ")
+	if err != nil {
+		log.Fatalf("error marshalling events into JSON \n all events: %v ", e)
+	}
+	fmt.Println(string(eventsJson))
+}
+
+func ChargeToJSON(c *commerce.ChargeResponse) {
+	chargeJson, err := json.MarshalIndent(c, "", " ")
+	if err != nil {
+		log.Fatalf("error marshalling response into JSON: %s \n. charge response: %v", err, c)
+	}
+	fmt.Printf("charge: %s", string(chargeJson))
+
+}
+
+func BuildCharge(v string) *commerce.ChargeRequest {
+	c := &commerce.ChargeRequest{
+		PricingType: "fixed_price",
+		LocalPrice: &commerce.LocalPrice{
+			Amount:   v,
+			Currency: "USD",
+		},
+	}
+	return c
+}
