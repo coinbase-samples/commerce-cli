3164da00a70410c04743b2436cf938fc00af21b9
Patrick Hughes <patrick.hughes@coinbase.com>
2024-02-21 14:54:07 -0600

    check for flags then validate

--- cmd/charges.go
+++ cmd/charges.go
@@ -6,12 +6,13 @@ import (
 	"time"
 
 	"github.com/coinbase-samples/commerce-cli/sdk"
-
 	"github.com/spf13/cobra"
 )
 
 var amount string
 var chargeId string
+var get bool
+var create bool
 
 var chargesCmd = &cobra.Command{
 	Use:   "charges",
@@ -22,34 +23,39 @@ var chargesCmd = &cobra.Command{
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
-		if sdk.Client == nil {
-			log.Fatalf("client not initialized")
-		}
-
-		if cmd.Flag("setPrice").Changed && cmd.Flag("get").Changed {
-			log.Fatalf("cannot have both a price and charge id")
-		} else if cmd.Flag("setPrice").Changed {
+		if create {
+			if amount == "" {
+				log.Fatal("Please specify a price with --price when creating a charge")
+			}
 			chargeReq := BuildCharge(amount)
 			resp, err := sdk.Client.CreateCharge(ctx, chargeReq)
 			if err != nil {
 				log.Fatalf("error creating charge: %s ", err)
 			}
 			ChargeToJSON(resp)
-		} else if cmd.Flag("get").Changed {
+			return
+		}
+
+		if get {
+			if chargeId == "" {
+				log.Fatal("Please specify a charge ID with --id when retrieving a charge")
+			}
 			charge, err := sdk.Client.GetCharge(ctx, chargeId)
 			if err != nil {
-				log.Fatalf("Error obtaining charge: %s - error: %s\n", chargeId, err)
+				log.Fatalf("Error obtaining charge: %s - error: %s", chargeId, err)
 			}
 			ChargeToJSON(charge)
-		} else {
-			log.Fatalf("Please provide either --setPrice (-p) or --get (-g) flag.")
+			return
 		}
+		log.Fatal("Please specify an action --create or --get")
 
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(chargesCmd)
+	chargesCmd.Flags().BoolVar(&create, "create", false, "Create a new charge")
+	chargesCmd.Flags().BoolVar(&get, "get", false, "Retrieve an existing charge by its id")
 	chargesCmd.Flags().StringVarP(&amount, "create", "p", "", "Set the price for a charge")
-	chargesCmd.Flags().StringVarP(&chargeId, "get", "g", "", "Retrieve a charge by its id")
+	chargesCmd.Flags().StringVar(&chargeId, "id", "", "Retrieve a charge by its id")
 }
