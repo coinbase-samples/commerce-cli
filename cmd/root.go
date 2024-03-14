90b41fd98e673412fdec609b16a4e2b1ae934ae2
Patrick Hughes <patrick.hughes@coinbase.com>
2024-03-14 18:55:11 -0500

    move client init into (root.go)

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,27 @@
 package cli
 
 import (
+	"log"
+	"net/http"
 	"os"
 
+	"github.com/coinbase-samples/commerce-sdk-go"
 	"github.com/spf13/cobra"
+)
+
+var Client *commerce.Client
+
+const (
+	COMMERCE_API_KEY = "COMMERCE_API_KEY"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "commerce",
 	Short: "Commerce is a CLI for managing e-commerce operations.",
 	Long:  `A fast and flexible CLI built with Cobra for managing e-commerce operations.`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		return InitClient()
+	},
 }
 
 func Execute() {
@@ -23,5 +35,14 @@ func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	rootCmd.AddCommand(eventsCmd)
 	rootCmd.AddCommand(chargesCmd)
+}
 
+func InitClient() error {
+	creds, err := commerce.ReadEnvCredentials(COMMERCE_API_KEY)
+	if err != nil {
+		log.Fatalf("error reading environmental variable: %s", err)
+	}
+
+	Client = commerce.NewClient(creds, http.Client{})
+	return nil
 }
--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	cli "github.com/coinbase-samples/commerce-cli/cmd"
-
-	"github.com/coinbase-samples/commerce-cli/sdk"
 )
 
 func main() {
-	sdk.InitClient()
 	cli.Execute()
 }
