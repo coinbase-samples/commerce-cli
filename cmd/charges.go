10896087d19cfd12756e5aa9c77a02499981e087
Patrick Hughes <patrick.hughes@coinbase.com>
2024-03-14 18:16:41 -0500

    create a subcommand for charges

--- cmd/charges.go
+++ cmd/charges.go
@@ -9,10 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var amount string
 var chargeId string
-var get bool
-var create bool
 
 var chargesCmd = &cobra.Command{
 	Use:   "charges",
@@ -23,23 +20,7 @@ var chargesCmd = &cobra.Command{
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
-		if create {
-			if amount == "" {
-				log.Fatal("Please specify a price with --price when creating a charge")
-			}
-			chargeReq := BuildCharge(amount)
-			resp, err := sdk.Client.CreateCharge(ctx, chargeReq)
-			if err != nil {
-				log.Fatalf("error creating charge: %s ", err)
-			}
-			ChargeToJSON(resp)
-			return
-		}
-
-		if get {
-			if chargeId == "" {
-				log.Fatal("Please specify a charge ID with --id when retrieving a charge")
-			}
+		if chargeId != "" {
 			charge, err := sdk.Client.GetCharge(ctx, chargeId)
 			if err != nil {
 				log.Fatalf("Error obtaining charge: %s - error: %s", chargeId, err)
@@ -47,15 +28,13 @@ var chargesCmd = &cobra.Command{
 			ChargeToJSON(charge)
 			return
 		}
-		log.Fatal("Please specify an action --create or --get")
+
+		log.Fatal("Please specify an action `create` or provide an --id to retrieve a charge")
 
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(chargesCmd)
-	chargesCmd.Flags().BoolVar(&create, "create", false, "Create a new charge")
-	chargesCmd.Flags().BoolVar(&get, "get", false, "Retrieve an existing charge by its id")
-	chargesCmd.Flags().StringVarP(&amount, "amount", "p", "", "Set the price for a charge")
 	chargesCmd.Flags().StringVar(&chargeId, "id", "", "Retrieve a charge by its id")
 }
---
+++ cmd/create.go
@@ -0,0 +1,48 @@
+package cli
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/coinbase-samples/commerce-cli/sdk"
+	"github.com/spf13/cobra"
+)
+
+var currency string
+var redirect string
+var chargeType string
+var amount string
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a new charge",
+	Long:  CreateLongDescription,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if amount == "" {
+			log.Fatal("Please specify a price with --amount or -p when creating a charge")
+		}
+
+		chargeReq := BuildCharge(chargeType, amount, currency, redirect)
+		resp, err := sdk.Client.CreateCharge(ctx, chargeReq)
+		if err != nil {
+			log.Fatalf("error creating charge: %s ", err)
+		}
+		ChargeToJSON(resp)
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(chargesCmd)
+	chargesCmd.AddCommand(createCmd)
+	createCmd.Flags().StringVarP(&amount, "amount", "p", "", "Set amount to charge")
+	createCmd.Flags().StringVarP(&redirect, "redirect", "r", "", "URL to redirect to after charge creation")
+	createCmd.Flags().StringVarP(&currency, "currency", "c", "", "Currency of the charge (ex: USD)")
+	createCmd.Flags().StringVarP(&chargeType, "type", "t", "fixed_price", "Type of the charge: 'fixed' or 'none'")
+	createCmd.MarkFlagRequired("amount")
+}
